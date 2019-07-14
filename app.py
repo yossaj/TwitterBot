8d77e60d08acdf4c6cd12a742c1313dfeed748c9
yossaj <yossaj@icloud.com>
2019-07-14 21:49:04 +0100

    now running to login

--- app.py
+++ app.py
@@ -1,18 +1,25 @@
 from selenium import webdriver
-from selenium.webdriver.common.keys import keys
+from selenium.webdriver.common.keys import Keys
 import time
 
 class TwitterBot:
     def __init__(self,username,password):
         self.username = username 
         self.password = password
-        self.bot = webdriver.Firebox()
+        self.bot = webdriver.Firefox()
 
     def login(self):
         bot = self.bot
-        bot.get('http://twitter.com')
+        bot.get('http://www.twitter.com/')
         time.sleep(3)
+        email = bot.find_element_by_class_name('email-input')
+        password = bot.find_element_by_name('session[password]')
+        email.clear()
+        password.clear()
+        email.send_keys(self.username)
+        password.send_keys(self.password)
+        password.send_keys(Keys.RETURN)
 
 
 doggo = TwitterBot('Name', 'password')
-doggo.login
+doggo.login()
